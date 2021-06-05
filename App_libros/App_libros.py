aac4470eee1cbe3189ae930c162cb65816827f89
FranciscoBa2 <franciscobagli5@gmail.com>
2021-06-05 00:55:42 -0300

    Merge branch 'master' of https://github.com/FranciscoBa2/Proyecto_final
    # Conflicts:
    #	App_libros/App_libros.py
    #	App_libros/Ingreso_datos_porconsola.py

--- App_libros/App_libros.py
+++ App_libros/App_libros.py
@@ -6,14 +6,14 @@ class cliente:
     def __init__(self, Nombre, Apellido, id_number, contrasenia):
         self.Nombre = Nombre
         self.Apellido = Apellido
-        self.id_number = str(id_number)
         self.contrasenia = contrasenia
+        self.id_number = id_number
 
     def agregar_clientes(self):
 
         conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
         cursor = conexion.cursor()
-        sentencia = "SELECT * FROM clientes WHERE id_number = " + "'" + self.id_number + "'"
+        sentencia = "SELECT * FROM clientes WHERE contrasenia = '" + self.contrasenia + "' and  id_number = '" + self.id_number + "'"
         cursor.execute(sentencia)
         cliente = cursor.fetchone()
         if cliente is None:
@@ -28,7 +28,7 @@ class cliente:
     def eliminar_clientes(self):
         conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
         cursor = conexion.cursor()
-        sentencia1 = "DELETE FROM clientes WHERE id_number = " + "'" + self.id_number + "' and contrasenia = " + "'" + self.contrasenia + "'"
+        sentencia1 = "DELETE FROM clientes WHERE contrasenia = '" + self.contrasenia + "' and id_number = '" + self.id_number + "'"
         cursor.execute(sentencia1)
         conexion.commit()
         conexion.close()
@@ -48,14 +48,17 @@ class libros:
     def agregar_libros(self):
         conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
         cursor = conexion.cursor()
-        sentencia = "SELECT * FROM Libros WHERE id_number = " + "'" + self.id_number + "' and titulo_obra = " + "'" + self.titulo_obra + "'"
-        cursor.execute(sentencia)
+        sentencia1 = "SELECT * FROM clientes WHERE id_number = '" + self.id_number + "'"
+        sentencia2 = "SELECT * FROM Libros WHERE titulo_obra = '" + self.titulo_obra + "' and id_number = '" + self.id_number + "'"
+        cursor.execute(sentencia1)
+        id = cursor.fetchone()
+        cursor.execute(sentencia2)
         Libro = cursor.fetchone()
-        if Libro is None:
+        if Libro is None and id is not None:
             conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
             cursor = conexion.cursor()
             x = (self.titulo_obra, self.genero, self.paginas, self.precio, self.id_number)
-            sentencia_sql = "INSERT INTO clientes VALUES (?, ?, ?, ?, ?)"
+            sentencia_sql = "INSERT INTO Libros VALUES (?, ?, ?, ?, ?)"
             cursor.executemany(sentencia_sql, [x])
             conexion.commit()
             conexion.close()
@@ -86,14 +89,17 @@ class libros_usados(libros):
     def agregar_libros_usados(self):
         conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
         cursor = conexion.cursor()
-        sentencia = "SELECT * FROM Libros_usados WHERE id_number = " + "'" + self.id_number + "' and titulo_obra = " + "'" + self.titulo_obra + "'"
-        cursor.execute(sentencia)
+        sentencia1 = "SELECT * FROM clientes WHERE id_number = '" + self.id_number + "'"
+        sentencia2 = "SELECT * FROM Libros_usados WHERE id_number = " + "'" + self.id_number + "' and titulo_obra = " + "'" + self.titulo_obra + "'"
+        cursor.execute(sentencia1)
+        id = cursor.fetchone()
+        cursor.execute(sentencia2)
         Libro = cursor.fetchone()
-        if Libro is None:
+        if Libro is None and id is not None:
             conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
             cursor = conexion.cursor()
             n = (self.titulo_obra, self.genero, self.paginas, self.precio, self.id_number, self.condicion, self.estado, self.tiempo_de_uso)
-            sentencia_sql = "INSERT INTO clientes VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+            sentencia_sql = "INSERT INTO libros_usados VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
             cursor.executemany(sentencia_sql, [n])
             conexion.commit()
             conexion.close()
--- App_libros/Ingreso_datos_porconsola.py
+++ App_libros/Ingreso_datos_porconsola.py
@@ -20,13 +20,13 @@ def consulta_de_libros():
                 titulo_obra = input('Nombre del libro: ')
                 conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
                 cursor = conexion.cursor()
-                sentencia = "SELECT * FROM Libros WHERE titulo_obra = " + "'" + titulo_obra + "'"
+                sentencia = "SELECT * FROM Libros WHERE titulo_obra " + "LIKE '%" + titulo_obra + "%'"
                 cursor.execute(sentencia)
                 cliente = cursor.fetchone()
                 if cliente is None:
                     conexion = sqlite3.connect('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/Applibros.db')
                     cursor = conexion.cursor()
-                    sentencia = "SELECT * FROM Libros_usados WHERE titulo_obra = " + "'" + titulo_obra + "'"
+                    sentencia = "SELECT * FROM Libros_usados WHERE titulo_obra " + "lIKE '%" + titulo_obra + "%'"
                     cursor.execute(sentencia)
                     cliente = cursor.fetchone()
                 if cliente is None:
@@ -78,15 +78,15 @@ def registro_clientes():
     print('Elija una de las siguientes opciones:')
     print('1- Alta cliente')
     print('2 - Baja cliente')
-    print('3- Modificacion de datos cliente')
+    print('3- ingreso de libros')
     move = input('tipo de movimiento: ')
     if move == '1':
         movimiento = 'Alta'
         fecha = datetime.now()
         nombre = input('nombre: ')
         apellido = input('apellido: ')
-        id_number = input('id_number: ')
         contrasenia = input('contrasenia: ')
+        id_number = input('id_number: ')
 
         with open('C:/Users/franc/PycharmProjects/Practicas fundamentos/App_libros/app.libros.csv', 'a', newline='\n') as archivo:
             campos = ['movimiento', 'fecha', 'nombre', 'apellido', 'id_number', 'contrasenia', 'libros']
@@ -101,21 +101,66 @@ def registro_clientes():
     elif move == '2':
         movimiento = 'Baja'
         fecha = datetime.now()
-        id_number = input('id_number: ')
         contrasenia = input('contrasenia: ')
+        id_number = input('id_number: ')
 
         with open('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/app.libros.csv', 'a',
                   newline='\n') as archivo:
-            campos = ['movimiento', 'fecha', 'nombre', 'apellido', 'id_number', 'contrasenia', 'libros']
+            campos = ['movimiento', 'fecha', 'nombre', 'apellido', 'contrasenia']
             writer = csv.DictWriter(archivo, fieldnames=campos)
             writer.writerow({
-                'movimiento': movimiento, 'fecha': fecha, 'nombre': '', 'apellido': '',
-                'id_number': id_number, 'contrasenia': contrasenia
+                'movimiento': movimiento, 'fecha': fecha, 'nombre': '', 'apellido': '', 'contrasenia': contrasenia
             })
         cliente_a_eliminar = cliente(Nombre='', Apellido='', id_number=id_number, contrasenia=contrasenia)
         cliente_a_eliminar.eliminar_clientes()
 
-#
+    elif move == '3':
+        print('que tipo de libro deeas insertar?')
+        print('1- nuevo')
+        print('2- usado')
+        opcion = input('opcion a elegir: ')
+        if opcion == '1':
+            movimiento = 'Modificacion'
+            fecha = datetime.now()
+            id_number = input('id_number: ')
+            titulo_obra = input('titulo_obra: ')
+            genero = input('contrasenia: ')
+            paginas = input('paginas: ')
+            precio = input('precio: ')
+
+            with open('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/app.libros.csv',
+                      'a',
+                      newline='\n') as archivo:
+                campos = ['movimiento', 'fecha', 'nombre', 'apellido', 'contrasenia']
+                writer = csv.DictWriter(archivo, fieldnames=campos)
+                writer.writerow({
+                    'movimiento': movimiento, 'fecha': fecha, 'nombre': '', 'apellido': '', 'contrasenia': ''
+                })
+            libro_a_agregar = libros(titulo_obra=titulo_obra, genero=genero, paginas=paginas, precio=precio, id_number=id_number)
+            libro_a_agregar.agregar_libros()
+
+        if opcion == '2':
+            movimiento = 'Modificacion'
+            fecha = datetime.now()
+            id_number = input('id_number: ')
+            titulo_obra = input('titulo_obra: ')
+            genero = input('contrasenia: ')
+            paginas = input('id_number: ')
+            precio = input('precio: ')
+            condicion = input('condicion: ')
+            estado = input('estado: ')
+            tiempo_de_uso = input('tiempo_de_uso: ')
+
+            with open('C:/Users/franc/PycharmProjects/Practicas fundamentos/Proyecto_final/App_libros/app.libros.csv',
+                      'a',
+                      newline='\n') as archivo:
+                campos = ['movimiento', 'fecha', 'nombre', 'apellido', 'contrasenia']
+                writer = csv.DictWriter(archivo, fieldnames=campos)
+                writer.writerow({
+                    'movimiento': movimiento, 'fecha': fecha, 'nombre': '', 'apellido': '', 'contrasenia': ''
+                })
+            libro_u_agregar = libros_usados(titulo_obra=titulo_obra, genero=genero, paginas=paginas, precio=precio, id_number=id_number, condicion=condicion, estado=estado, tiempo_de_uso=tiempo_de_uso)
+            libro_u_agregar.agregar_libros_usados()
 #
 #
 # cliente1 = registro_clientes()
